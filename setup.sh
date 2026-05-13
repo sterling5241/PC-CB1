3fc6a2d46cc230e14dcab04bb79807e4ed8a3e50
sterling5241 <sterling5241@gmail.com>
2026-05-13 00:08:11 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,11 +33,33 @@ apt-get install -y git python3 python3-pip python3-venv virtualenv nginx curl wg
 cd /home/biqu
 sudo -u biqu git clone https://github.com/dw-0/kiauh.git
 
-# Auto install Klipper, Moonraker, Mainsail, Crowsnest
-sudo -u biqu /home/biqu/kiauh/kiauh.sh -- install klipper
-sudo -u biqu /home/biqu/kiauh/kiauh.sh -- install moonraker
-sudo -u biqu /home/biqu/kiauh/kiauh.sh -- install mainsail
-sudo -u biqu /home/biqu/kiauh/kiauh.sh -- install crowsnest
+# Run all installs as biqu
+sudo -u biqu bash <<'BIQU'
+
+cd /home/biqu
+
+# Klipper
+git clone https://github.com/Klipper3d/klipper.git
+./klipper/scripts/install-debian.sh
+
+# Moonraker
+git clone https://github.com/Arksine/moonraker.git
+./moonraker/scripts/install-moonraker.sh
+
+# Mainsail
+mkdir -p /home/biqu/mainsail
+cd /home/biqu/mainsail
+wget -q -O mainsail.zip https://github.com/mainsail-crew/mainsail/releases/latest/download/mainsail.zip
+unzip -q mainsail.zip
+rm mainsail.zip
+cd /home/biqu
+
+# Crowsnest
+git clone https://github.com/mainsail-crew/crowsnest.git
+cd crowsnest && make install
+cd /home/biqu
+
+BIQU
 
 echo "=== Setup Complete ==="
 echo "Access Mainsail at: http://$(hostname -I | awk '{print $1}')"
