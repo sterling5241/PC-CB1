1e0c91ea6f7aaa7d9c90b61ed8273b03e574be27
sterling5241 <sterling5241@gmail.com>
2026-05-12 20:39:27 -0500

    Add build-minipc.sh for compiling bigtreetech-cb1
    This script clones a repository, downloads configuration files, and compiles the project for the bigtreetech-cb1 board.

---
+++ build-minipc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/sterling5241/build --branch bpi-main --depth=1
+cd build
+
+curl -o config/boards/bigtreetech-cb1.conf \
+  https://raw.githubusercontent.com/sterling5241/PC-CB1/main/build/config/boards%20/bigtreetech-cb1.conf
+
+curl -o lib/functions/rootfs/distro-agnostic.sh \
+  https://raw.githubusercontent.com/sterling5241/PC-CB1/main/lib/functions/rootfs/distro-agnostic.sh
+
+./compile.sh \
+  BOARD=bigtreetech-cb1 \
+  BRANCH=current \
+  RELEASE=trixie \
+  BUILD_MINIMAL=yes \
+  BUILD_DESKTOP=no \
+  KERNEL_CONFIGURE=no
